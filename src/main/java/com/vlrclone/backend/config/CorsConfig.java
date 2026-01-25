45e8d260a46d80996610533680f09b01afce315c
Note2022 <wrongpass789@gmail.com>
2026-01-25 20:03:49 +0000

    try render 1.3

--- src/main/java/com/vlrclone/backend/config/CorsConfig.java
+++ src/main/java/com/vlrclone/backend/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000",
+                                "https://note-np63.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
