d284937d305793513b1f7fb2b32c57ab2ec0033d
Note2022 <wrongpass789@gmail.com>
2026-02-11 18:23:09 +0000

    New Button in FrontEnd for Tasks

--- frontend/src/FrontPage.js
+++ frontend/src/FrontPage.js
@@ -11,13 +11,26 @@ export default function FrontPage() {
                 <p>
                     Discover events, sports, and communities — all in one place.
                 </p>
+                <div className={"f-buttons"}>
+                    <button
+                        className="enter-btn"
+                        onClick={() => (window.location.hash = `#/home`)}
+                    >
+                        Home
+                    </button>
 
-                <button
-                    className="enter-btn"
-                    onClick={() => (window.location.hash = `#/home`)}
-                >
-                    Enter
-                </button>
+                    <button
+                        className="enter-btn"
+                        onClick={() => (window.location.hash = `#/usability-tasks`)}
+                    >
+                        Tasks
+                    </button>
+                </div>
+                <p></p>
+                <p>
+                    Find Tasks: <br></br>Click <b>"Tasks"</b> or Look at <b> Header -> SideNavigation -> Usability Tasks</b>
+                </p>
+
             </div>
         </div>
     );
